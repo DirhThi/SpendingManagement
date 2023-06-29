0a01d66a3f99e003853eddf30055b8dff6dbefc9
truongducthien2k2 <truongducthien2k2@gmail.com>
2023-06-30 00:00:08 +0700

    fix bug and add data

--- navigation/AppTabsNavigator.js
+++ navigation/AppTabsNavigator.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import NAVIGATION_KEY from '../constans/NavigationKey';
 import HomeScreen from '../src/screens/HomeScreen';
-import User from '../src/screens/User';
 import { MyTabBar } from './MyTabBar';
-import { Ionicons } from '@expo/vector-icons';
-import { useTheme } from 'native-base';
+import { Ionicons,FontAwesome,AntDesign,FontAwesome5 } from '@expo/vector-icons';
+import { useTheme,Button } from 'native-base';
 import TransactionScreen from '../src/screens/Transaction/transaction';
+import SamplePopupScreen from '../src/screens/SamplePopupScreen';
+import AddTranSactionScreen from '../src/screens/AddTransaction/addtransaction';
+import UserScreen from '../src/screens/User';
+import ReportScreen from '../src/screens/Report/report';
 
 const BottomTab = createBottomTabNavigator();
 
-export default function AppTabsNavigator() {
+export default function AppTabsNavigator({navigation}) {
     const { colors } = useTheme();
     return (
         <BottomTab.Navigator
@@ -18,7 +21,9 @@ export default function AppTabsNavigator() {
             tabBar={(props) => <MyTabBar {...props} />}
             screenOptions={{
                 headerShadowVisible: false,
+                
             }}
+            
         >
             <BottomTab.Screen
                 name={NAVIGATION_KEY.Home}
@@ -26,30 +31,66 @@ export default function AppTabsNavigator() {
                 options={{
                     title: 'Trang chủ',
                     tabBarIcon: () => <Ionicons name="home-outline" size={24} color={colors.primary[500]} />,
+                    
                 }}
             />
             <BottomTab.Screen
                 name="transaction"
                 component={TransactionScreen}
+                
                 options={{
+                    
                     title: 'Giao dịch',
-                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color={colors.primary[500]} />,
+                    tabBarIcon: () => <AntDesign name="calendar" size={24} color={colors.primary[500]} />,
                 }}
             />
             <BottomTab.Screen
-                name="TestScreen2"
-                component={HomeScreen}
+                name="addTransaction"
+                component={AddTranSactionScreen}
+                
                 options={{
-                    title: 'Test Screen 2',
-                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color={colors.primary[500]} />,
+                    title: "Thêm giao dich",
+                    tabBarLabel:null,
+                    tabBarIcon:  () => (
+                        <Button
+                        justifyContent={'center'}
+                        
+                          borderRadius={200}
+                          bottom="30%"
+                          h={16}
+                          w={16}
+                          style={{shadowColor: '#000000',
+                          shadowOffset: {
+                            width: 0,
+                            height: 2
+                          },
+                          shadowRadius: 5,
+                          shadowOpacity: 0.7}}
+                          onPress={() => navigation.navigate('addtransaction')}
+                        >
+                        <FontAwesome  name="plus" size={28} color="white" />                        
+                        </Button>
+                      ),
                 }}
             />
             <BottomTab.Screen
-                name="User"
-                component={User}
+                name="report"
+                component={ReportScreen}
+            
                 options={{
+                    
+                    title: 'Báo cáo',
+                    tabBarIcon: () => <FontAwesome5 name="chart-bar" size={24} color={colors.primary[500]} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="user"
+                component={UserScreen}
+                
+                options={{
+                    
                     title: 'Người dùng',
-                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color={colors.primary[500]} />,
+                    tabBarIcon: () => <FontAwesome name="user-o" size={24} color={colors.primary[500]} />,
                 }}
             />
         </BottomTab.Navigator>
